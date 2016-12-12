aafe051146ea9a38068d2a27dceac2746b7472c9
Jacqueline Liu <jacqliu@mit.edu>
2016-12-11 21:03:42 -0500

    global params

--- code/project-code/trail.py
+++ code/project-code/trail.py
@@ -13,7 +13,7 @@ import random
 from common.gfxutil import *
 
 time_len = 150
-now_bar_loc = 150
+now_bar_loc = 100
 fwoosh_time = .2
 shape_time = 3
 top_diff = 10
@@ -23,12 +23,10 @@ lanes_width = None #Window.width/2 - 50
 now_bar_width = None #(lanes_height-now_bar_loc)/float(lanes_height)*(bottom_diff-top_diff) + top_diff
 
 def redraw_window(): 
-    lanes_height = Window.height/2 + 100
-    lanes_width = Window.width/2 - 50
+    global lanes_height, lanes_width, now_bar_width
+    lanes_height = Window.height/4 * 3
+    lanes_width = Window.width/2
     now_bar_width = (lanes_height-now_bar_loc)/float(lanes_height)*(bottom_diff-top_diff) + top_diff
-    
-
-
 
 class TrailDisplay(InstructionGroup):
     def __init__(self): #callback function takes in which shape was created
